5f5f6fa738b44163e0eab4e39f98073838fedb71
Илья Смаль <user925@realwebad.air>
2017-01-26 18:23:22 +0300

    development page is almost ready

---
+++ __dev/scripts/project/development.js
@@ -0,0 +1,15 @@
+// App.gui.add({
+//     _name: 'development-masonry',
+//         selector: '.js-development-masonry',
+//         build: function($el){
+//         var options = {
+//             itemSelector: '.js-development-masonry-item',
+//             gutter: 20,
+//             columnWidth: '.grid-sizer'
+//         };
+//         $el.masonry(options);
+//     },
+//     destroy: function($el, methods){
+//         if(methods && typeof methods['destroy'] == 'function') methods['destroy']();
+//     }
+// });
