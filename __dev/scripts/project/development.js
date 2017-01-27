6cb557e27182b4b0d9b0b2bba24fd350d73ef385
Илья Смаль <user925@realwebad.air>
2017-01-27 18:19:28 +0300

    some fixes of pages

--- __dev/scripts/project/development.js
+++ __dev/scripts/project/development.js
@@ -13,3 +13,12 @@
 //         if(methods && typeof methods['destroy'] == 'function') methods['destroy']();
 //     }
 // });
+// App.run(function() {
+//     $(document).ready(function () {
+//         // Inject YouTube API script
+//         var tag = document.createElement('script');
+//         tag.src = "//www.youtube.com/player_api";
+//         var firstScriptTag = document.getElementsByTagName('script')[0];
+//         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//     });
+// });
