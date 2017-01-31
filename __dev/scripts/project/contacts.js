2b1235f6583dd666cf4aafd47b0531ae123a8dff
Илья Смаль <user925@realwebad.air>
2017-01-31 18:21:22 +0300

    contacts is almost ready

---
+++ __dev/scripts/project/contacts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    ymaps.ready(init);
+
+    function init() {
+        var maps = $('.js-map');
+        if(maps.length < 1) return;
+
+        var mapsArr = [];
+
+        maps.each(function(id) {
+            mapsArr[id] = new ymaps.Map("contacts-shop-map-"+id, {
+                center: [59.958169564129435,30.313164000000015],
+                zoom: 17
+            });
+
+            mapsArr[id].geoObjects.add(new ymaps.Placemark([59.958169564129435,30.313164000000015]));
+        });
+    }
+});
